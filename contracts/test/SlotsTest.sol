0fb95543157bf6c8bd627ecc2995d18c74310e15
belbix <7453635@gmail.com>
2022-06-14 09:49:21 +0300

    initial commit

---
+++ contracts/test/SlotsTest.sol
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.4;
+
+import "../proxy/ControllableV3.sol";
+
+contract SlotsTest is ControllableV3 {
+  struct SomeStruct {
+    uint a;
+    uint b;
+  }
+
+  bytes32 internal constant _SLOT = bytes32(uint256(keccak256("slot")) - 1);
+
+  mapping(uint => SomeStruct) public map;
+
+  function initialize(address controller_) external initializer {
+    __Controllable_init(controller_);
+  }
+
+  function setMapA(uint index, uint a) external {
+    map[index].a = a;
+  }
+
+  function getBytes32() external view returns (bytes32 result) {
+    return SlotsLib.getBytes32(_SLOT);
+  }
+
+  function getAddress() external view returns (address result) {
+    return SlotsLib.getAddress(_SLOT);
+  }
+
+  function getUint() external view returns (uint result) {
+    return SlotsLib.getUint(_SLOT);
+  }
+
+  function arrayLength() external view returns (uint result) {
+    return SlotsLib.arrayLength(_SLOT);
+  }
+
+  function addressAt(uint index) external view returns (address result) {
+    return SlotsLib.addressAt(_SLOT, index);
+  }
+
+  function uintAt(uint index) external view returns (uint result) {
+    return SlotsLib.uintAt(_SLOT, index);
+  }
+
+  function setByte32(bytes32 value) external {
+    SlotsLib.set(_SLOT, value);
+  }
+
+  function setAddress(address value) external {
+    SlotsLib.set(_SLOT, value);
+  }
+
+  function setUint(uint value) external {
+    SlotsLib.set(_SLOT, value);
+  }
+
+  function setAt(uint index, address value) external {
+    SlotsLib.setAt(_SLOT, index, value);
+  }
+
+  function setAt(uint index, uint value) external {
+    SlotsLib.setAt(_SLOT, index, value);
+  }
+
+  function setLength(uint length) external {
+    SlotsLib.setLength(_SLOT, length);
+  }
+
+  function push(address value) external {
+    SlotsLib.push(_SLOT, value);
+  }
+
+}
+
+
+/// @dev extending SomeStruct with new member
+contract SlotsTest2 is ControllableV3 {
+  struct SomeStruct {
+    uint a;
+    uint b;
+  }
+
+  mapping(uint => SomeStruct) public map;
+
+  function initialize(address controller_) external initializer {
+    __Controllable_init(controller_);
+  }
+
+  function setMapA(uint index, uint a) external {
+    map[index].a = a;
+  }
+
+  function setMapB(uint index, uint b) external {
+    map[index].b = b;
+  }
+
+}
---
+++ test/lib/SlotsTest.ts
@@ -0,0 +1,115 @@
+import {ethers} from "hardhat";
+import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
+import {expect} from "chai";
+import {DeployerUtils} from "../../scripts/utils/DeployerUtils";
+import {
+  ControllerMinimal, ProxyControlled, ProxyControlled__factory,
+  SlotsTest,
+  SlotsTest2,
+  SlotsTest2__factory,
+  SlotsTest__factory
+} from "../../typechain";
+import {formatBytes32String} from "ethers/lib/utils";
+import {TimeUtils} from "../TimeUtils";
+
+describe("Slots Tests", function () {
+  let snapshotBefore: string;
+  let snapshot: string;
+  let signer: SignerWithAddress;
+  let slotsTest: SlotsTest;
+  let controller: ControllerMinimal;
+
+  before(async function () {
+    snapshotBefore = await TimeUtils.snapshot();
+    [signer] = await ethers.getSigners();
+    controller = await DeployerUtils.deployMockController(signer);
+
+    const logic = await DeployerUtils.deployContract(signer, 'SlotsTest');
+    const proxy = await DeployerUtils.deployContract(signer, 'ProxyControlled') as ProxyControlled;
+    await proxy.initProxy(logic.address);
+    slotsTest = SlotsTest__factory.connect(proxy.address, signer);
+    await slotsTest.initialize(controller.address);
+
+    expect(await ProxyControlled__factory.connect(proxy.address, signer).implementation()).eq(logic.address)
+  });
+
+  after(async function () {
+    await TimeUtils.rollback(snapshotBefore);
+  });
+
+
+  beforeEach(async function () {
+    snapshot = await TimeUtils.snapshot();
+  });
+
+  afterEach(async function () {
+    await TimeUtils.rollback(snapshot);
+  });
+
+
+  it("Slots returns same as sets after proxy upgrade", async () => {
+    const values = [11, 22, 33, 44, 55];
+    for (let i = 0; i < values.length; i++) {
+      console.log('set A', i, values[i]);
+      await slotsTest.setMapA(i, values[i]);
+    }
+
+    console.log('deploy SlotsTest2 logic');
+    const slotsTest2Impl = await DeployerUtils.deployContract(signer, 'SlotsTest2') as SlotsTest2;
+    await expect(controller.updateProxies([slotsTest.address], [signer.address])).revertedWith('UpgradeableProxy: new implementation is not a contract');
+    await controller.updateProxies([slotsTest.address], [slotsTest2Impl.address]);
+    const slotsTest2 = SlotsTest2__factory.connect(slotsTest.address, signer);
+
+    // write to new B member to check A will not rewrite
+    const mulB = 100;
+    for (let i = 0; i < values.length; i++) {
+      const val = values[i] * mulB;
+      console.log('set B', i, val);
+      await slotsTest2.setMapB(i, val);
+    }
+
+    for (let i = 0; i < values.length; i++) {
+      const slotStruct = await slotsTest2.map(i);
+      console.log('get struct', i, slotStruct);
+      expect(slotStruct.a).is.eq(values[i])
+      expect(slotStruct.b).is.eq(values[i] * mulB)
+    }
+
+  });
+
+  it("byte32 test", async () => {
+    await slotsTest.setByte32(formatBytes32String('test'))
+    expect(await slotsTest.getBytes32()).eq(formatBytes32String('test'))
+  });
+
+  it("address test", async () => {
+    await slotsTest.setAddress(signer.address)
+    expect(await slotsTest.getAddress()).eq(signer.address)
+  });
+
+  it("uint test", async () => {
+    await slotsTest.setUint(11)
+    expect(await slotsTest.getUint()).eq(11)
+  });
+
+  it("array length test", async () => {
+    await slotsTest.setLength(11)
+    expect(await slotsTest.arrayLength()).eq(11)
+  });
+
+  it("array address test", async () => {
+    await slotsTest["setAt(uint256,address)"](1, signer.address)
+    expect(await slotsTest.addressAt(1)).eq(signer.address)
+  });
+
+  it("array uint test", async () => {
+    await slotsTest["setAt(uint256,uint256)"](1, 11)
+    expect(await slotsTest.uintAt(1)).eq(11)
+  });
+
+  it("array push test", async () => {
+    await slotsTest.push(signer.address)
+    expect(await slotsTest.addressAt(0)).eq(signer.address)
+  });
+
+})
